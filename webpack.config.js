0650fa51cd66e645360581631cbea26825f1032e
Lincoln Chawora <Lincoln_1996@hotmail.com>
2023-05-05 16:06:51 +0100

    Set up dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-
 module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css')
@@ -9,7 +8,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: "bundle.js"
         },
         module: {
@@ -43,7 +42,8 @@ module.exports = (env) => {
         devtool: isProduction ? "source-map" : "inline-source-map",
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true // this means that 404's will result in the index page being served
+            historyApiFallback: true, // this means that 404's will result in the index page being served
+            publicPath: '/dist'
         }
     }
 }
